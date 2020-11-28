7defab3b714e2ef7c20fddddbd884391f5736569
Krikrifreeze <krikrifreeze@gmail.com>
2020-11-27 22:46:47 -0800

    Successfully moved old page to react

---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import './Projects.css';
+import githubLogo from './images/github.png';
+
+/*
+import Link from 'react-router-dom';
+*/
+import projectData from './projectList.json';
+
+
+export class Item extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name : this.props.name,
+            description : this.props.description,
+            github : this.props.github
+        }
+
+    }
+
+    render(){
+
+        return(
+            <div class = 'showcaseItem'>
+                <div class = 'item-header'>
+                    {this.state.name}
+                </div>
+
+                <div class ='item-body'>
+                    {this.state.description}
+                </div>
+
+                <div class = 'item-footer'>
+                    <div class = 'item-link'>
+                        <img class = "item-link-icon" src={githubLogo} />
+                        <a href={this.state.github} class ='item-link-name'>
+                            GITHUB
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export class Projects extends React.Component {
+
+    render(){
+
+        //map the projectData to Item Components
+        const projects = projectData.projects.map( (p, index) => {
+            return(
+                <Item
+                    name={p.name}
+                    description={p.description}
+                    github={p.github}
+                />
+            )
+        })
+
+
+        return(
+            <div class = "things">
+                <div id="showcase">
+                    <b><p>PROJECTS AND SCRIPTS</p></b>
+                    <div> 
+                        { projects }
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Projects;
