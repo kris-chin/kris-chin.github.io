eecc1dbceb286007d9f411602df53100802cd82b
Krischin Layon <krischinlayon@gmail.com>
2022-01-21 14:02:53 -0800

    refactor CubeCircle

--- src/ts/behaviours/CubeCircle.ts
+++ src/ts/behaviours/CubeCircle.ts
@@ -10,29 +10,31 @@ export class CubeCircle extends Behaviour{
 
     Get(){}
 
-    Step(){
-        if (!this.firstRun){
-            if (this.base && this.base.mesh){
+    //Code for first run
+    Init(){
+        if (this.base && this.base.mesh){
 
-                let params = this.parameters as CubeCircleParams
+            let params = this.parameters as CubeCircleParams
 
-                //Creates 30 Cubes
-                for (let i = 0; i < 30; i++){
+            //Creates 30 Cubes
+            for (let i = 0; i < 30; i++){
 
-                    let cuber = this.base.world.AddObject('cube',{'threeParent':this.base.mesh});
-        
-                    if (cuber && cuber.mesh){
-                        cuber.mesh.position.set( Math.cos(i/30 * 2*Math.PI) * 2, Math.sin(i/30 * 2*Math.PI) * 2, 0)
-                        cuber.mesh.rotation.z = ((Math.random() - 0.5) * 2) * 2;
-                    }
-                    
+                let cuber = this.base.world.AddObject('cube',{'threeParent':this.base.mesh});
+    
+                if (cuber && cuber.mesh){
+                    cuber.mesh.position.set( Math.cos(i/30 * 2*Math.PI) * 2, Math.sin(i/30 * 2*Math.PI) * 2, 0)
+                    cuber.mesh.rotation.z = ((Math.random() - 0.5) * 2) * 2;
                 }
-
-                this.firstRun = true;
-
-                this.base.mesh.rotateX(params.rotation)
+                
             }
+            this.base.mesh.rotateX(params.rotation)
+            this.firstRun = true;
         }
+    }
+
+    //Code called every frame
+    Step(){
+        if (!this.firstRun) this.Init()
     }
 
 }
